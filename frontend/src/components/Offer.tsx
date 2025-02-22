fb114d32a923146e5ef997885b0d01036152a3e4
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-02-22 19:16:12 +0545

    add offer heading

--- frontend/src/components/Offer.tsx
+++ frontend/src/components/Offer.tsx
@@ -1,8 +1,21 @@
+import { NavLink } from "react-router"
 
 
 const Offer = () => {
   return (
-    <div>Offer</div>
+    <div className="bg-black text-white w-full h-10 flex-center text-sm ">
+      <div className="flex-center gap-2 ">
+        <h1 >Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</h1>
+        <NavLink to="/product-details/swimsuits" className="font-semibold underline">ShopNow</NavLink>
+      </div>
+      <div className="ml-52">
+        <select name="language" id="language" className="bg-black text-white curosr-pointer">
+          <option value="eng">English</option>
+          <option value="nep">Nepali</option>
+        </select>
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import Offer from "../components/Offer"
 
 
 const Layout = () => {
   return (
     <>
+      <Offer />
       <Navbar />
       <Outlet />
       <Footer />
