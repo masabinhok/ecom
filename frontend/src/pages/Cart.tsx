19a3f7bfa69a47046b00b15d8d9e67d1a15afa88
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-02-26 22:22:02 +0545

    feat: enhance Cart page layout with buttons for shopping and updating cart; add coupon code input and cart total summary

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -23,7 +23,41 @@ const Cart = () => {
             ))
           }
         </div>
-       
+      </section>
+      <div className="flex items-center justify-between mt-10">
+        <button className="border-2 border-black-200 px-10 py-3 rounded-sm text-sm ">
+          Continue Shopping
+        </button>
+        <button className="border-2 border-black-200 px-10 py-3 rounded-sm text-sm ">
+          Update Cart
+        </button>
+      </div>
+      <section className="flex justify-between items-start mt-20">
+        <div className="flex items-center gap-5">
+          <input type="text" className="border-2 border-black-200 w-[300px] py-3 px-4 text-sm" placeholder="Coupon Code" />
+          <button className="text-white text-sm bg-brand px-10 py-3 rounded-sm ">
+            Apply Coupon
+          </button>
+        </div>
+        <div className="border-2 border-black-700 max-w-[400px] rounded-sm w-full p-5 text-sm flex flex-col gap-5">
+          <h1 className="font-medium text-lg">Cart Total</h1>
+          <div className="flex justify-between pb-1 border-b-2 border-black-200 text-sm">
+            <p>Subtotal:</p>
+            <p>$3000</p>
+          </div>
+          <div className="flex justify-between pb-1 border-b-2 border-black-200 text-sm">
+            <p>Shipping:</p>
+            <p>Free</p>
+          </div>
+          <div className="flex justify-between  text-sm">
+            <p>Total:</p>
+            <p>$3000</p>
+          </div>
+          <button className="text-white text-sm bg-brand px-6 py-3 rounded-sm self-center w-fit ">
+            Proceed to Checkout
+          </button>
+
+        </div>
       </section>
     </main>
   )
