0e20b2f5b04c72a82f9f7829dfedead308523dca
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-02-26 22:35:07 +0545

    feat: add ScrollToTop component to reset scroll position on route change; integrate it into Layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Offer from "../components/Offer"
+import ScrollToTop from "../components/ScrollToTop"
 
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Offer />
       <Navbar />
       <main className="w-full flex justify-center mt-34 min-h-screen">
