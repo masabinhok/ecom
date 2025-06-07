811adad23c9f985e07a9515b1974b342ea14fcdd
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-06-07 23:32:46 +0545

    feat: enhance wishlist functionality with add, remove, and move to cart methods; update schema for unique userId in Wishlist

--- backend/src/wishlists/wishlists.module.ts
+++ backend/src/wishlists/wishlists.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WishlistsService } from './wishlists.service';
 import { WishlistsController } from './wishlists.controller';
+import { CartsModule } from 'src/carts/carts.module';
 
 @Module({
+  imports: [CartsModule],
   controllers: [WishlistsController],
   providers: [WishlistsService],
 })
--- backend/src/wishlists/wishlists.service.ts
+++ backend/src/wishlists/wishlists.service.ts
@@ -1,5 +1,73 @@
 import { Injectable } from '@nestjs/common';
+import { CartItem, WishlistItem } from 'generated/prisma';
+import { CartsService } from 'src/carts/carts.service';
+import { AddToCartDto } from 'src/carts/dtos/add-to-cart.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class WishlistsService {constructor(private prisma: PrismaService){}}
+export class WishlistsService {
+  constructor(private prisma: PrismaService, 
+    private cartsService: CartsService
+  ){}
+
+  async addToWishlist(userId: number, productId: number){
+    // check is wishlist exists or not?
+    let wishlist = await this.prisma.wishlist.findUnique({
+      where: {
+        userId
+      },
+      include: {
+        items: true,
+      }
+    });
+
+
+    // if no wishlist, create one,
+    if(!wishlist){
+      wishlist = await this.prisma.wishlist.create({
+        data: {
+          userId,
+        },
+        include: {
+          items: true,
+        }
+      })
+    }
+
+    const existingItem = await this.prisma.wishlistItem.findFirst({
+      where: {
+        wishlistId: wishlist.id,
+        productId
+      }
+    })
+
+    if(existingItem){
+      throw new Error('Item already in wishlist!');
+    }
+
+    return this.prisma.wishlistItem.create({
+      data: {
+        wishlistId: wishlist.id,
+        productId,
+      }
+    });
+  }
+
+  async removeFromWishlist(wishlistItemId: number): Promise<WishlistItem>{
+      const updatedWishlist = await this.prisma.wishlistItem.delete({
+          where: {
+            id: wishlistItemId
+          }
+      });
+      if(!updatedWishlist){
+        throw new Error('Item not found!')
+      }
+      return updatedWishlist;
+  }
+
+  async moveOneItemToCart(userId: number, wishlistItemId: number, addToCartDto: AddToCartDto): Promise<CartItem>{
+    await this.removeFromWishlist(wishlistItemId);
+    return this.cartsService.addToCart(userId, addToCartDto);
+  }
+
+}
